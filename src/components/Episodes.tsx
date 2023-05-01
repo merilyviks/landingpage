618402b66031304c806caabb357b8463564b4863
merilyviks <73937690+merilyviks@users.noreply.github.com>
2023-05-01 09:37:30 +0300

    Update Episodes.tsx

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -5,28 +5,28 @@ export default function Episodes() {
   const episodes = [
     {
       id: 1,
-      name: "edu võti on...",
+      name: "1. Edu võti on...",
       url: "/guest1.jpg",
       description:
         "...enesedisipliinist, selle tähendusest ning selle saavutamisest",
     },
     {
       id: 2,
-      name: "ettevõtlus vs startup",
+      name: "2. Ettevõtlus vs startup",
       url: "/guest2.jpg",
       description:
         "Mis vahet on klassikalisel ettevõtlusel ja startup-il. Ettevõtte perspektiivist on rääkimas täna Mari Tamm ja startupist Laura Põldmaa. Kumba oleks lihtsam alustada?",
     },
     {
       id: 3,
-      name: "hea une saladus",
+      name: "3. Hea une saladus",
       url: "/guest3.jpg",
       description:
         "Uneta me ei saa, kuid kas magamine on nii keeruline kui tundub. Kas tead, kuidas head unekvaliteeti tagada?",
     },
     {
       id: 4,
-      name: "kas Ameerikas on päriselt hea elada?",
+      name: "4. Kas Ameerikas on päriselt hea elada?",
       url: "/guest4.jpg",
       description:
         "Külas on meil Ameerikalne, kellega arutame plussidest ja miinustest välismaal. Kas pärastel ka käivad lapsed kuulikindla seljakotiga koolis?",
